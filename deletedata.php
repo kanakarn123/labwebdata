f046dcb00a3308b0218c05db41a99fec6d02c052
kanakarn123 <73680257+kanakarn123@users.noreply.github.com>
2020-11-19 21:58:13 +0700

    Add files via upload

--- deletedata.php
+++ deletedata.php
@@ -6,6 +6,13 @@ if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL PLEASE TRY AGAIN: '.mysqli_connect_error());
 }
+
+$name = $_POST['name'];
+$comment = $_POST['comment'];
+
+
+$sql = "DELETE FROM guestbook WHERE Name='$name'";
+
 if (mysqli_query($conn, $sql)) {
   header("Location: showdata.php");
   } else {
